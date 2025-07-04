e2c85070f246d647b2bc401a542bd1b8698f5256
Abdullah Alhussainan <abdullah.alhussainan@gmail.com>
2025-07-04 18:48:38 +0300

    Arabic QWERTY Physical keyboard version one

---
+++ app/src/main/java/com/negalabs/arabicqwertykeyboard/InputDeviceReceiver.kt
@@ -0,0 +1,11 @@
+package com.negalabs.arabicqwertykeyboard
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InputDeviceReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
